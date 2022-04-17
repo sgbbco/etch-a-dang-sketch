832b894606111cf2a19e4cc0eaffb7557288d4ea
Mr-Robot-83 <scott.bonanno@gmail.com>
2022-04-17 22:09:01 +1000

    added mouse trail, drawing and clear funcitons

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const canvasContainer = document.querySelector('.canvas-container');
 const slider = document.querySelector('#grid-slider');
 const gridSizeLabel = document.querySelector('.grid-size-label');
+const shakeButton = document.querySelector('#shake-button');
 
 //Variable ratio - wanted to experiment with a wide format
 const gridRatio = 2; //Ratio of 1:gridRatio
 
+let mouseDown = false
+canvasContainer.onmousedown = () => {mouseDown = true; console.log("Mouse is down")};
+canvasContainer.onmouseup = () => {mouseDown = false; console.log("Mouse is up")};
+
+let currentSize = 25;
 
 //Input default canvas settings
-defaultCanvas(15,5,50)//starting rows, min rows , max rows
+defaultCanvas(currentSize,5,50)//starting rows, min rows , max rows
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function sliderInput() {
   gridSizeLabel.innerHTML = `Grid size: ${this.value} x ${this.value * gridRatio}`;
+  currentSize = this.value;
   changeGridSize(this.value);
-  console.log(this);
-}
+};
+
 
 
 //Setup default canvas
@@ -24,7 +31,7 @@ function defaultCanvas(size,min,max) {
   slider.setAttribute('min',min);
   slider.setAttribute('max',max);
   gridSizeLabel.innerHTML = `Grid size: ${size} x ${size * gridRatio}`; 
-} 
+};
 
 function changeGridSize(gridSize) {
   setupNewCanvas(gridSize);
@@ -36,17 +43,44 @@ function setupNewCanvas(gridSize) {
   canvasContainer.innerHTML = '';
   canvasContainer.style.gridTemplateColumns = `repeat(${gridSize * gridRatio}, 1fr)`;
   canvasContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-}
+};
 
 //Add the correct amount of divs to the grid
 function addSquares(gridSize) {
-  for (let i=1; i <= gridSize * (gridSize * gridRatio); i++) {
+  for (let i = 1; i <= gridSize * (gridSize * gridRatio); i++) {
   const gridSquare = document.createElement('div');
   gridSquare.classList.add('grid-square');
   gridSquare.id = `Sq${i}`;
+  gridSquare.addEventListener('mousedown', colourSquare);
+  gridSquare.addEventListener('mouseover', mousetrail);
   canvasContainer.appendChild(gridSquare);
-  }
-}
+  };  
+};
+
+shakeButton.addEventListener('click',shakeCanvas);
+
+function shakeCanvas() {
+  changeGridSize(currentSize);
+  canvasContainer.classList.add('canvas-shake');
+  canvasContainer.addEventListener('animationend', ()=>{
+    canvasContainer.classList.remove('canvas-shake');
+    console.log("animation over")
+   });
+};
+
+function colourSquare(e) {
+  e.target.classList.add('draw');
+};
+
+function mousetrail(e) {
+ if (mouseDown === true) {
+  e.target.classList.add('draw');
+ }
+ e.target.classList.add('hover');
+ e.target.addEventListener('transitionend', ()=>{
+   e.target.classList.remove('hover')
+  });
+};
 
 
 
